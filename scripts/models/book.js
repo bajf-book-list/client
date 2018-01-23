c69aee4e8a4e90ef50fb74d2de9a36feeda79ef2
Joshua Fredrickson <uafredrickson@gmail.com>
2018-01-23 10:02:45 -0800

    added aboutUs, containers, book Constructor

--- scripts/models/book.js
+++ scripts/models/book.js
@@ -2,42 +2,61 @@
 // var _CLIENT_URL = 'https://bajf-book-list.github.io/client' 
 var __API_URL__ = 'https://bajf-book-list.herokuapp.com'
 
-pageLoad();
+(function(module) {
 
-$('#user-form').on('submit', function(e) {
-  e.preventDefault();
-
-  let data = {
-    name: e.target.name.value,
-    age: e.target.age.value,
-    ninja: e.target.ninja.value
-  }
-
-  $.post(`${__API_URL__}/api/v1/books`, data)
-  .then(function() {
     pageLoad();
-  })
-  .catch(function(err) {
-    console.error(err);
-    pageLoad();
-  });
-});
 
-function pageLoad() {
-  $.get(`${__API_URL__}/api/v1/books`)
-  .then(function(data) {
-    console.log('our data:', data);
-    $('#results').empty();
+    Book.prototype.toHtml = function() {
+        var template = Handlebars.compile($('#book-list-template').text());
 
-    data.rows.forEach(function(item) {
-      let content = `
-        <h2>name: ${item.name}</h2>
-        <p>age: ${item.age}</p>
-        <p>ninja status: ${item.ninja}</p>
-      `;
-      $('#results').append(content);
+        // put stuff here!!!
+
+        return template(this);
+    }
+
+
+
+
+
+
+
+
+    $('#user-form').on('submit', function(e) {
+    e.preventDefault();
+
+    let data = {
+        name: e.target.name.value,
+        age: e.target.age.value,
+        ninja: e.target.ninja.value
+    }
+
+    $.post(`${__API_URL__}/api/v1/books`, data)
+    .then(function() {
+        pageLoad();
+    })
+    .catch(function(err) {
+        console.error(err);
+        pageLoad();
     });
-  }, function(err) {
-    console.error(err);
-  });
-}
+    });
+
+    function pageLoad() {
+    $.get(`${__API_URL__}/api/v1/books`)
+    .then(function(data) {
+        console.log('our data:', data);
+        $('#results').empty();
+
+        data.rows.forEach(function(item) {
+        let content = `
+            <h2>name: ${item.name}</h2>
+            <p>age: ${item.age}</p>
+            <p>ninja status: ${item.ninja}</p>
+        `;
+        $('#results').append(content);
+        });
+    }, function(err) {
+        console.error(err);
+    });
+    }
+
+})(window)
--- scripts/views/book-view.js
+++ scripts/views/book-view.js
@@ -1,9 +1,11 @@
 'use strict';
+(function(module){
 
-function Book(title, author, image_url,isbn) {
-    this.title = title;
-    this.author = author;
-    this.image_url = image_url;
-    this.isbn = isbn;
-}
-
+    function Book(title, author, image_url,isbn) {
+        this.title = title;
+        this.author = author;
+        this.image_url = image_url;
+        this.isbn = isbn;
+    }
+    
+})(window)
