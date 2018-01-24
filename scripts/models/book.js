a9b9bddfabf05281fb934d19c4210870ce29b915
Bessie Arino <brarino@gmail.com>
2018-01-24 08:52:24 -0800

    preparing files for production

--- scripts/models/book.js
+++ scripts/models/book.js
@@ -1,19 +1,17 @@
 'use strict';
 
 // var _CLIENT_URL = 'https://bajf-book-list.github.io/client' 
-// for production: var __API_URL__ = 'https://bajf-book-list.herokuapp.com'
-var __API_URL__ = '' // for test environment
+var __API_URL__ = 'https://bajf-book-list.herokuapp.com' // for production
+// var __API_URL__ = '' // for test environment
 
-// (function(module) {
-
-    pageLoad();
+(function(module) {
 
     function Book(title, author, image_url,isbn) {
         this.title = title;
         this.author = author;
         this.image_url = image_url;
         this.isbn = isbn;
-    }
+    };
 
     Book.all = [];
 
@@ -23,10 +21,7 @@ var __API_URL__ = '' // for test environment
         // put stuff here!!!
 
         return template(this);
-    }
-
-
-
+    };
 
     Book.loadAll = rows => {
         rows.sort((a, b) => (b.title) - (a.title))
@@ -43,48 +38,49 @@ var __API_URL__ = '' // for test environment
         })
     };
 
+    module.Book = Book;
+})(window);
 
+pageLoad();
+    
+$('#user-form').on('submit', function(e) {
+    e.preventDefault();
 
-    $('#user-form').on('submit', function(e) {
-        e.preventDefault();
-
-        let data = {
-            title: e.target.title.value,
-            author: e.target.author.value,
-            image_url: e.target.image_url.value,
-            isbn: e.target.isbn.value,
-            description: e.target.description.value
-        }
-
-        $.post(`${__API_URL__}/api/v1/books`, data)
-        .then(function() {
-            pageLoad();
-        })
-        .catch(function(err) {
-            console.error(err);
-            pageLoad();
-        });
-    });
-
-    function pageLoad() {
-    $.get(`${__API_URL__}/api/v1/books`)
-    .then(function(data) {
-        console.log('our data:', data);
-        $('#results').empty();
-
-        data.rows.forEach(function(item) {
-        let content = `
-            <h2>title: ${item.title}</h2>
-            <p>age: ${item.author}</p>
-            <p>image_url: ${item.image_url}</p>
-            <p>isbn: ${item.isbn}</p>
-            <p>description: ${item.description}</p>
-        `;
-        $('#results').append(content);
-        });
-    }, function(err) {
-        console.error(err);
-    });
+    let data = {
+        title: e.target.title.value,
+        author: e.target.author.value,
+        image_url: e.target.image_url.value,
+        isbn: e.target.isbn.value,
+        description: e.target.description.value
     }
 
-// })(window)
+    $.post(`${__API_URL__}/api/v1/books`, data)
+    .then(function() {
+        pageLoad();
+    })
+    .catch(function(err) {
+        console.error(err);
+        pageLoad();
+    });
+});
+
+function pageLoad() {
+$.get(`${__API_URL__}/api/v1/books`)
+.then(function(data) {
+    console.log('our data:', data);
+    $('#results').empty();
+
+    data.rows.forEach(function(item) {
+    let content = `
+        <h2>title: ${item.title}</h2>
+        <p>age: ${item.author}</p>
+        <p>image_url: ${item.image_url}</p>
+        <p>isbn: ${item.isbn}</p>
+        <p>description: ${item.description}</p>
+    `;
+    $('#results').append(content);
+    });
+},  function(err) {
+    console.error(err);
+    });
+};
--- scripts/views/book-view.js
+++ scripts/views/book-view.js
@@ -1,6 +1,7 @@
 'use strict';
-(function(module){
+
+(function(module) {
 
 
     
-})(window)
+})(window);
